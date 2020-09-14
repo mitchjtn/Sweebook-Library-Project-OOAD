7057c8748071f31dee38672e740a99f498b8ee91
Michael Jonathan <michael.jonathan01@gmail.com>
2020-09-14 16:26:47 +0700

    last

--- src/view/LogInView.java
+++ src/view/LogInView.java
@@ -94,7 +94,7 @@ public class LogInView extends JFrame implements ActionListener {
 			
 			username = txtUsername.getText();
 			password = String.valueOf(txtPassword.getPassword());
-			//password = User.encryptPassword(password);
+			password = User.encryptPassword(password);
 			UserHandler userH = new UserHandler();
 			user = userH.getByUsername(username); 
 			
